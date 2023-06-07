a480891ff30832fba94841f1a80b05760e39208f
Pranali Thorat <100304254+Pranali34@users.noreply.github.com>
2023-06-07 18:02:32 +0530

    Add files via upload

---
+++ Basic_Programs/Self_Information.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+          printf("\n\n ******************SELF INFORMATION********************\n\n");
+
+          printf("\n\t:> Name = Thorat Pranali Hanmant\n");
+
+          printf("\n\t:> Course = BCA1 \n");
+
+          printf("\n\t:> Rollno. = 184 \n");
+
+          printf("\n\t:> Div = B \n");
+
+          printf("\n\t:> College = SGM College,Karad \n");
+
+          printf("\n\t:> Address  =A/p - Sawade, Tal - Karad, Dist - Satara \n");
+
+          printf("\n\n **********************THANK YOU************************\n");
+
+          getch();
+          return 0;
+}
